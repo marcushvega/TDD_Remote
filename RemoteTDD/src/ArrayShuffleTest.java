1b124e8c2a7676ad26af7007a5d40f666c2300e2
Alex-Kaumeheiwa <titokaumeheiwa@gmail.com>
2019-11-11 19:11:18 -0500

    First Test Added

--- RemoteTDD/src/ArrayShuffleTest.java
+++ RemoteTDD/src/ArrayShuffleTest.java
@@ -1,5 +1,19 @@
-package PACKAGE_NAME;
+// A simple example to get you started
+// JUnit assertion - the default Java assertion library
+// https://junit.org/junit5/
+
+import static org.junit.Assert.assertEquals;
+
+import PACKAGE_NAME.ArrayShuffle;
+import org.junit.Test;
+//import org.junit.jupiter.api.Test;
+
 
 public class ArrayShuffleTest
 {
+    @Test
+    public void CreateArray(){
+        int[] intArr = new int[] {1,2};
+        assertEquals(intArr, ArrayShuffle.createArray());
+    }
 }
