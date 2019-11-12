f62bf49d662ffb373199d93b13073696577a68ab
Alex-Kaumeheiwa <titokaumeheiwa@gmail.com>
2019-11-11 19:39:14 -0500

    Created the Compare Array Method

--- RemoteTDD/src/ArrayShuffle.java
+++ RemoteTDD/src/ArrayShuffle.java
@@ -1,4 +1,4 @@
-//package PACKAGE_NAME;
+
 
 public class ArrayShuffle
 {
@@ -7,5 +7,14 @@ public class ArrayShuffle
         array[0] = 1;
         array[1] = 2;
         return array;
+    }
+
+    public static boolean equalArrays(int[] expected, int[] actual){
+        for(int i = 0; i< actual.length; i++){
+            if(actual[i] != expected[i]){
+                return false;
+            }
+        }
+        return true;
     }
 }
--- RemoteTDD/src/ArrayShuffleTest.java
+++ RemoteTDD/src/ArrayShuffleTest.java
@@ -4,7 +4,7 @@
 
 import static org.junit.Assert.assertEquals;
 
-//import PACKAGE_NAME.ArrayShuffle;
+
 import org.junit.Test;
 //import org.junit.jupiter.api.Test;
 
