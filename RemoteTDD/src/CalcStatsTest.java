22c4b2b3f45da48a8b286aaf275ae420183960dc
Alex-Kaumeheiwa <titokaumeheiwa@gmail.com>
2019-11-26 18:12:14 -0500

    created another test case for maxValue meathod. Created test for counting values in the array

--- RemoteTDD/src/CalcStatsTest.java
+++ RemoteTDD/src/CalcStatsTest.java
@@ -10,21 +10,21 @@ public class CalcStatsTest {
 
 
     @Test
-    void test_for_minimum_value() {
+    public void test_for_minimum_value() {
         int expected = 1;
         int[] input = new int[] {1};
         assertEquals(expected, CalcStats.min(input));
     }
 
     @Test
-    void test_for_creation_of_CalcStats_object() {
+    public void test_for_creation_of_CalcStats_object() {
         int[] input = new int[] {1};
         CalcStats obj = new CalcStats(input);
         assertNotNull(obj);
     }
 
     @Test
-    void CSObject_minimumValue() {
+    public void CSObject_minimumValue() {
         int expected = 1;
         int[] input = new int[] {1,2};
         CalcStats obj = new CalcStats(input);
@@ -32,17 +32,32 @@ public class CalcStatsTest {
     }
 
     @Test
-    void CSObject_minimumValue_when_array_has_three_values() {
+    public void CSObject_minimumValue_when_array_has_three_values() {
         CalcStats obj1 = new CalcStats(new int[] {5,4,0});
         assertEquals(0, obj1.getMin());
     }
 
     @Test
-    void CSObject_maxValue(){
+    public void CSObject_maxValue(){
         int expected = 2;
         int[] input = new int[] {1,2};
         CalcStats obj = new CalcStats(input);
         assertEquals(expected, obj.getMax());
+    }
 
+    @Test
+    public void CSObject_maxValueWithFourValues(){
+        int expected = 8;
+        int[] input = new int[] {1,5,2,8};
+        CalcStats obj = new CalcStats(input);
+        assertEquals(expected, obj.getMax());
+    }
+
+    @Test
+    public void CSObject_countValues(){
+        int expected = 1;
+        int[] input = new int[] {3};
+        CalcStats obj = new CalcStats(input);
+        assertEquals(expected, obj.countValues());
     }
 }
